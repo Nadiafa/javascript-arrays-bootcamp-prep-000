0a8cbbbab42411fcb7be40347a50eb686d65cda3
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-25 17:07:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  var chocolateBars = [element, ...array]
+  chocolateBars = [element, ...array]
   return chocolateBars
   
 }
