d68e5c19c81a7d68a0e2484b76d2bffae3d7ded0
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-25 18:11:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,7 +23,8 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.slice()
+  array.shift()
+  return array
 }
 
 function removeElementFromBeginningOfArray(array) {
