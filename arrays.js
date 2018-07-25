0980515388a8c63940117e8542bd0c2d019220e4
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-25 17:03:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,12 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  var additionalChocolateBars = [element, ...chocolateBars]
+  var additionalChocolateBars = [${element}, ...chocolateBars]
   return additionalChocolateBars
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  var chocolateBars = [element, ...chocolateBars]
+  var chocolateBars = [${element}, ...chocolateBars]
   return chocolateBars
   
 }
