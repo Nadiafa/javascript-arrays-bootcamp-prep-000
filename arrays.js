a0f6ef06b2da55f0a78339d4788136db9d1ba525
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-25 16:57:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  var additionalChocolateBars = ['Mars', ...chocolateBars]
+  var additionalChocolateBars = ["Mars", ...chocolateBars]
   return additionalChocolateBars
 }
 
